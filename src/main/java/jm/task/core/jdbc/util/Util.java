6b6d597dacafd0831a0f4962ace3a5b01e3290a9
Artur <ishmaev49@gmail.com>
2022-06-06 11:44:08 +0500

    Run
    Run

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,17 +7,14 @@ import java.sql.SQLException;
 public class Util {
 
     // реализуйте настройку соеденения с БД
-    private static final String HOSTNAME = "localhost";
-    private static final String PORT = "3306";
-    private static final String NAME_DB = "katadb_1_1_3";
     private static final String USERNAME = "user1";
     private static final String PASSWORD = "kata_113";
+    private static final String URL = "jdbc:mysql://localhost:3306/katadb_1_1_3";
 
     public static Connection getConnection() {
         Connection connection = null;
-        String connectionLine = "jdbc:mysql://" + HOSTNAME + ":" + PORT + "/" + NAME_DB;
         try {
-            connection = DriverManager.getConnection(connectionLine, USERNAME, PASSWORD);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
             e.printStackTrace();
         }
